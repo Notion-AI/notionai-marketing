4fd848d0f00f693f4a7997cb59b1306d3309d386
hoangbitatech <hoang@bitatech.co>
2022-10-16 14:52:43 +0700

    resovled conflic

--- components/Benefits.vue
+++ components/Benefits.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="benefits">
         <div class="container mx-auto">
-            <h2 class="benefits__title">
+            <h2 v-if="type === 'air'" class="air benefits__title">
                 <p>How AiR benefits</p>
                 <p>your business.</p>
+            </h2>
+
+            <h2 v-if="type === 'stream'" class="stream benefits__title">
+                <p>How Stream benefits</p>
+                <p>you as a professional.</p>
             </h2>
 
             <div class="benefits__content grid grid-cols-1 md:grid-cols-3">
                 <BenefitsItem
                     v-for="(i, idx) in benefits" :key="idx"
+                    :class="type"
                     :img="i.img"
                     :title="i.title"
                     :gradient="i.gradient"
@@ -26,16 +32,38 @@ export default {
     components: { BenefitsItem },
     data() {
         return {
-            benefits: [
+            benefits: [],
+            benefitsAir: [
                 {img: '/images/benefit-1.png', title: 'Save', gradient: 'Resources', subOne: 'Do more with less in support of', subSecond: 'growth and efficiency initiatives.'},
                 {img: '/images/benefit-2.png', title: 'Innovate', gradient: 'Faster', subOne: 'Extend the breadth of new ideas', subSecond: 'from global practices.'},
                 {img: '/images/benefit-3.png', title: 'Research', gradient: 'Repository', subOne: 'Repository of research that', subSecond: 'can be updated on demand.'}
+            ],
+
+            benefitsStream: [
+                {img: '/images/stream-dynamic-2.png', title: 'Access', gradient: 'Global Content', subOne: '100,000 enterprise-grade sources,', subSecond: 'one subscription.'},
+                {img: '/images/stream-dynamic-3.png', title: 'Unlimited', gradient: 'Briefings', subOne: 'Monitor big issues,', subSecond: 'personalised for you.'},
+                {img: '/images/stream-dynamic-4.png', title: 'Collaborate', gradient: 'Around Content', subOne: 'Understand and share the 1%', subSecond: 'that matters.'}
             ]
+        }
+    },
+
+    props: {
+        type: {
+            type: String,
+            default: 'air'
+        }
+    },
+
+    created() {
+        if(this.type === 'air') {
+            this.benefits = this.benefitsAir
+        } else {
+            this.benefits = this.benefitsStream
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-@import "~/assets/scss/components/air/benefits";
+@import "~/assets/scss/components/common/benefits";
 </style>
---
+++ components/Slogan.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="slogan">
+        <div class="container mx-auto">
+            <h2 v-if="type === 'air'" class="slogan__title air">
+                <p><span>AiR,</span></p>
+                <p>AI Research for teams</p>
+            </h2>
+
+            <h2 v-if="type === 'stream'" class="slogan__title stream">
+                <p><span>Stream,</span> dynamically</p>
+                <p>personalised briefing for you.</p>
+            </h2>
+
+            <div v-if="type === 'air'" class="slogan__sub">
+                <p>Radically improve your research process with</p>
+                <p>an AI-augmented research workflow.</p>
+            </div>
+
+            <div v-if="type === 'stream'" class="slogan__sub">
+                <p>A platform designed to give you direct access to a global source of content and</p>
+                <p>help you synthsise the themes, trends and changes affecting your organisation.</p>
+            </div>
+
+            <div class="slogan__btn">
+                <app-button
+                    class="mx-auto"
+                    color="outline-white"
+                    style="width: 190px;"
+                >Sign up for free trial</app-button>
+            </div>
+
+            <div class="slogan__img">
+                <img v-if="type === 'air'" src="~assets/images/air/ai.png" alt="" class="mx-auto" />
+                <img v-if="type === 'stream'" src="~assets/images/stream/stream-dynamic-1.png" alt="" class="mx-auto" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            type: String,
+            default: 'air'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/components/common/_slogan.scss";
+</style>
