160cdd251f9c883d95ff3811279cd2aa587e7b97
hoangbitatech <hoang@bitatech.co>
2022-10-19 01:21:56 +0700

    done page blog

--- components/blog/Sentences.vue
+++ components/blog/Sentences.vue
@@ -1,19 +1,46 @@
 <template>
     <div class="sentences">
-        <h2 class="sentences__title">
+        <h2 class="sentences__title lg:hidden">
+            <p>Sentence here about articles Notion publishes. <span class="text-gradient-yellow">Find what you’re looking for.</span></p>
+        </h2>
+
+        <h2 class="sentences__title hidden lg:block">
             <p>Sentence here about</p>
             <p>articles Notion publishes.</p>
             <p class="text-gradient-yellow">Find what you’re looking for.</p>
         </h2>
 
-        <div class="choose">
-            <span>Choose a from these categories</span>
-            <app-button nuxt to="">Press</app-button>
-            <app-button nuxt to="">Blog</app-button>
-            <app-button nuxt to="">Social</app-button>
-            <app-button nuxt to="">Events</app-button>
-            <app-button nuxt to="">Resources</app-button>
-            <app-button nuxt to="">All Topics</app-button>
+        <div class="sentences__choose">
+            <p class="choose-text">Choose a from these categories</p>
+            <app-button 
+                nuxt to=""
+                style="width: 74px"
+            >Press</app-button>
+
+            <app-button 
+                nuxt to=""
+                style="width: 74px"
+            >Blog</app-button>
+
+            <app-button 
+                nuxt to=""
+                style="width: 74px"
+            >Social</app-button>
+
+            <app-button 
+                nuxt to=""
+                style="width: 74px"
+            >Events</app-button>
+
+            <app-button 
+                nuxt to=""
+                style="width: 110px"
+            >Resources</app-button>
+
+            <app-button 
+                nuxt to=""
+                style="width: 100px; background: #000000; color: #fff"
+            >All Topics</app-button>
         </div>
     </div>
 </template>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="blog">
         <Sentences />
-        <ListArticle />
+        <ListArticle :data="list"/>
     </div>
 </template>
 
@@ -9,7 +9,60 @@
 import Sentences from '~/components/blog/Sentences.vue';
 import ListArticle from '../components/blog/ListArticle.vue';
 export default {
-    components: { Sentences, ListArticle }
+    components: { Sentences, ListArticle },
+    data() {
+        return {
+            list: [
+                {
+                    img: '/images/blog-1.png', 
+                    textBtn: 'Social', 
+                    title: 'Build a team dashboard to measure your startup team’s performance',
+                    text: "Having a dashboard helps keep your team aligned, and driving toward your goals. Here's how to build one.",
+                    time: '01 OCT 2022'
+                },
+
+                {
+                    img: '/images/blog-2.png', 
+                    textBtn: 'Resources', 
+                    title: 'Save senior leaders time by providing fit-for-person media monitoring.',
+                    text: "Sentence here that speaks to product feature. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    time: '01 OCT 2022'
+                },
+
+                {
+                    img: '/images/blog-3.png', 
+                    textBtn: 'Resources', 
+                    title: 'Build a team dashboard to measure your startup team’s performance',
+                    text: "Having a dashboard helps keep your team aligned, and driving toward your goals. Here's how to build one.",
+                    time: '01 OCT 2022'
+                },
+
+                {
+                    img: '/images/blog-4.png', 
+                    textBtn: 'Events', 
+                    title: 'Save senior leaders time by providing fit-for-person media monitoring.',
+                    text: "HSentence here that speaks to product feature. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    time: '01 OCT 2022'
+                },
+
+                {
+                    img: '/images/blog-5.png', 
+                    textBtn: 'Press', 
+                    title: 'Build a team dashboard to measure your startup team’s performance',
+                    text: "Having a dashboard helps keep your team aligned, and driving toward your goals. Here's how to build one.",
+                    time: '01 OCT 2022'
+                },
+
+                {
+                    img: '/images/blog-6.png', 
+                    textBtn: 'Social', 
+                    title: 'Save senior leaders time by providing fit-for-person media monitoring.',
+                    text: "Sentence here that speaks to product feature. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    time: '01 OCT 2022'
+                }
+            ]
+        }
+    }
 }
 </script>
 
