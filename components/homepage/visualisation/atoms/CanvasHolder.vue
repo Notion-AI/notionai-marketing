38b40fbb55cbc9f63ddabc5c52cdf046009f8d6e
Snow <snow@hol.ly>
2022-10-31 06:15:57 +1100

    NAC-1493 WIP Migrate into components.

---
+++ components/homepage/visualisation/atoms/CanvasHolder.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full h-screen overflow-hidden bg-black">
+    <slot />
+  </div>
+</template>
---
+++ components/homepage/visualisation/framework/Canvas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative w-full h-full overflow-hidden">
+    <div class="w-full h-full">
+      <canvas ref={{ canvasRef }} class="block" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      // camera: {
+
+      // },
+
+      canvasRef: {
+        type: String,
+        default: 'container'
+      },
+
+      // shadows: {
+
+      // }
+    },
+
+    mounted () {
+      console.log('Mounted')
+    }
+  }
+</script>
